a128d3a196be610760df666f4b2226d01b9980d6
CaptGuto <tadenafi@gmail.com>
2024-05-14 23:48:23 +0300

    some

--- src/About.js
+++ src/About.js
@@ -1,51 +1,53 @@
+import React from 'react';
+import imageCard1 from './assets/images/sesameseed.jpg'; 
+import aboutImage from './assets/images/grainVertical.jpg'; 
+import './App.css'
 
-import imageCard1 from './assets/images/sesameseed.jpg'
+const About = () => {
+  return (
+    <div className="About_Container">
 
+      <section className="About_Body_Container">
+        <div className="Content_Holder">
+          <div className="leftContent">
 
-function About(){
-    return(
-        <div className="About_Container">
-            <div className="About_Header_Container container-fluid">
-                <div className="Content_Holder container-fluid" >
-                    <h1>About DAK Trading PLC</h1>
-                    <h5>Something about DAK</h5>
+            <header className="About_Header_Container container-fluid">
+                <div className="Content_Holder container">
+                    <h1>About DAK </h1>
+                </div>
+            </header>
+            <div className='left_content_holder'>
+                <div className='left_content_paragraph '>
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, recusandae. Ex, molestiae qui. Eius,
+                    voluptates debitis atque totam optio modi fuga et inventore vel obcaecati veniam ipsam quaerat velit
+                    accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil asperiores minus
+                    harum, quos expedita nesciunt tempora. Quam voluptas maiores quas minima fugit at consequuntur et
+                    aliquam, ut necess itatibus? Reprehenderit!
+                    </p>
+                </div>
+                <div className='left_content_paragraph_image_holder'>
+                    <img src={imageCard1}></img>
+                </div>
+                <div className='left_content_paragraph '>
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, recusandae. Ex, molestiae qui. Eius,
+                    voluptates debitis atque totam optio modi fuga et inventore vel obcaecati veniam ipsam quaerat velit.
+                    </p>
                 </div>
             </div>
-            <div className="About_Body_Container">
-                <div className="Content_Holder">
-                    <div className='leftContent'>
-                        <h3>The Content on left</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, 
-                            recusandae. Ex, molestiae qui. Eius, voluptates debitis atque totam optio modi fuga et inventore vel 
-                            obcaecati veniam ipsam quaerat velit accusantium.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil asperiores minus harum, 
-                            quos expedita nesciunt tempora. Quam voluptas maiores quas minima fugit at 
-                            consequuntur et aliquam, ut necessitatibus? Reprehenderit!
-                        </p>
-                    </div>
-                    <div className="vertical-line"></div>
-                    <div className='rightContent'>
-                    <h3>The Content on Right</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, 
-                            recusandae. Ex, molestiae qui. Eius, voluptates debitis atque totam optio modi fuga et inventore vel 
-                            obcaecati veniam ipsam quaerat velit accusantium.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil asperiores minus harum, 
-                            quos expedita nesciunt tempora. Quam voluptas maiores quas minima fugit at 
-                            consequuntur et aliquam, ut necessitatibus? Reprehenderit!
-                        </p>
-                    </div>
-                    <div className='container-fluid'>
-{/* 
-                    <Cards cardTitle={"The Card Test"} 
-              cardSmallDescription={"a bit of something about the product in talk"} 
-                cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
-                  image={imageCard1}/> */}
-                    </div>
+          </div>
+          <div className="vertical-line1"></div>
+          <div className="vertical-line2"></div>
+          <div className="vertical-line3"></div>
 
-                </div>
-            </div>
+          <div className="rightContent">
+            <img src={aboutImage} alt="About DAK Trading PLC" /> 
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default About;
--- src/Contact.js
+++ src/Contact.js
@@ -35,7 +35,7 @@ function Contact(){
                         <p>something@gmail.com</p>
                     </div>
                 </div>          
-            <div className='col-lg-6 col-md-12 col-sm-12'>
+            <div className='col-lg-6 col-md-12 col-sm-12 p-0'>
                     <iframe src={'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3940.704117730342!2d38.74454407501858!3d8.999349891060813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwNTknNTcuNyJOIDM4wrA0NCc0OS42IkU!5e0!3m2!1sen!2set!4v1713292594980!5m2!1sen!2set'} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='map'></iframe>
             </div>
         </div>
