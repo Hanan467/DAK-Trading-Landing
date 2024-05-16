37c5c07512b76442a33d009763d9f2a559390f9d
hanan467 <hananansaresmael@gmail.com>
2024-05-16 14:08:14 +0300

    card and header updates

--- src/Service.js
+++ src/Service.js
@@ -6,26 +6,41 @@ import imageCard1 from './assets/images/grains2.png'
 function Service(){
     return(
         <div className="Service_Container">
-            <div className="Content_Holder">
-                <div className="listofServices">
-                    <div className="export_services">
-                        <div className='container-fluid'>
+            <div className=" row Content_Holder">
+                <h1 className="">Services</h1> 
+                <div className="row listofServices">
+                    <div className="row export_services">
+                        <div className='col-lg-4  container-fluid'>
+                            <Cards cardTitle={"The Card Test"} 
+                            cardSmallDescription={"a bit of something about the product in talk"} 
+                            cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+                            image={imageCard1} />
+                        </div>
+                        <div className='col-lg-4 container-fluid'>
                             <Cards cardTitle={"The Card Test"} 
                             cardSmallDescription={"a bit of something about the product in talk"} 
                             cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
                             image={imageCard1}/>
                         </div>
-                        <div className='container-fluid'>
+                        <div className='col-lg-4 container-fluid'>
                             <Cards cardTitle={"The Card Test"} 
                             cardSmallDescription={"a bit of something about the product in talk"} 
                             cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
                             image={imageCard1}/>
                         </div>
-                        <div className='container-fluid'>
-                            <Cards cardTitle={"The Card Test"} 
-                            cardSmallDescription={"a bit of something about the product in talk"} 
-                            cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
-                            image={imageCard1}/>
+                        <div class="row  mt-5" style={{marginLeft:'100px', marginTop:'80px'}}>
+                            <div className='col-lg-4  container-fluid'>
+                                <Cards cardTitle={"The Card Test"}
+                                cardSmallDescription={"a bit of something about the product in talk"}
+                                cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+                                image={imageCard1}/>
+                            </div>
+                            <div className='col-lg-4  container-fluid'>
+                                <Cards cardTitle={"The Card Test"}
+                                cardSmallDescription={"a bit of something about the product in talk"}
+                                cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+                                image={imageCard1}/>
+                            </div>
                         </div>
                     </div>
                 </div>
