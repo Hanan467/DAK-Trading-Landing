0f9ce77eddcb0faf9479c32239d2a629dd85ed6c
CaptGuto <tadenafi@gmail.com>
2024-05-17 12:26:17 +0300

    finished the service section responsivenes

--- src/Service.js
+++ src/Service.js
@@ -5,43 +5,39 @@ import imageCard1 from './assets/images/grains2.png'
 
 function Service(){
     return(
-        <div className="Service_Container">
-            <div className=" row Content_Holder">
-                <h1 className="">Services</h1> 
-                <div className="row listofServices">
-                    <div className="row export_services">
-                        <div className='col-lg-4  container-fluid'>
-                            <Cards cardTitle={"The Card Test"} 
-                            cardSmallDescription={"a bit of something about the product in talk"} 
-                            cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+        <div class="Service_Container">
+            <h1>Services</h1>
+            <div class="Content_Holder">
+                <div class="export_services row justify-content-center">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <Cards cardTitle={"The Card Test"} 
+                            cardSmallDescription={"A bit of something about the product in talk"} 
+                            cardDescription={"Writing a half Lorem ipsum just to depict the long description needed for this component to function properly"}
                             image={imageCard1} />
-                        </div>
-                        <div className='col-lg-4 container-fluid'>
-                            <Cards cardTitle={"The Card Test"} 
-                            cardSmallDescription={"a bit of something about the product in talk"} 
-                            cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <Cards cardTitle={"The Card Test"} 
+                            cardSmallDescription={"A bit of something about the product in talk"} 
+                            cardDescription={"Writing a half Lorem ipsum just to depict the long description needed for this component to function properly"}
                             image={imageCard1}/>
-                        </div>
-                        <div className='col-lg-4 container-fluid'>
-                            <Cards cardTitle={"The Card Test"} 
-                            cardSmallDescription={"a bit of something about the product in talk"} 
-                            cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <Cards cardTitle={"The Card Test"} 
+                            cardSmallDescription={"A bit of something about the product in talk"} 
+                            cardDescription={"Writing a half Lorem ipsum just to depict the long description needed for this component to function properly"}
                             image={imageCard1}/>
-                        </div>
-                        <div class="row  mt-5" style={{marginLeft:'100px', marginTop:'80px'}}>
-                            <div className='col-lg-4  container-fluid'>
-                                <Cards cardTitle={"The Card Test"}
-                                cardSmallDescription={"a bit of something about the product in talk"}
-                                cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
-                                image={imageCard1}/>
-                            </div>
-                            <div className='col-lg-4  container-fluid'>
-                                <Cards cardTitle={"The Card Test"}
-                                cardSmallDescription={"a bit of something about the product in talk"}
-                                cardDescription={"writing a half lorem jus tto dipict the long description needed for this component to function properly"}
-                                image={imageCard1}/>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <Cards cardTitle={"The Card Test"}
+                            cardSmallDescription={"A bit of something about the product in talk"}
+                            cardDescription={"Writing a half Lorem ipsum just to depict the long description needed for this component to function properly"}
+                            image={imageCard1}/>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <Cards cardTitle={"The Card Test"}
+                            cardSmallDescription={"A bit of something about the product in talk"}
+                            cardDescription={"Writing a half Lorem ipsum just to depict the long description needed for this component to function properly"}
+                            image={imageCard1}/>
                     </div>
                 </div>
             </div>
