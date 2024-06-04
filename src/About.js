dfad629347402966d3ae9f94c8f933ecfc5dbf62
CaptGuto <tadenafi@gmail.com>
2024-06-04 17:03:57 +0300

    create the second service page and rearanged something

--- src/About.js
+++ src/About.js
@@ -1,53 +1,114 @@
-import React from 'react';
-import imageCard1 from './assets/images/sesameseed.jpg'; 
-import aboutImage from './assets/images/grainVertical.jpg'; 
-import './App.css'
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import imageCard1 from "./assets/images/sesameseed.jpg";
+import aboutImage from "./assets/images/grainVertical.jpg";
+import "./App.css";
 
 const About = () => {
-  return (
-    <div className="About_Container">
+    const { ref: line1Ref, inView: line1InView } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: line2Ref, inView: line2InView } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: line3Ref, inView: line3InView } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: imageRef, inView: imageInView } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
 
-      <section className="About_Body_Container">
-        <div className="Content_Holder row">
-          <div className="leftContent col-sm-12">
-
-            <header className="About_Header_Container container-fluid col-sm-12">
-                <div className="Content_Holder container">
-                    <h1>About DAK </h1>
+    return (
+        <div className="About_Container">
+            <section className="About_Body_Container">
+                <div className="Content_Holder row">
+                    <div className="leftContent col-sm-12">
+                        <header className="About_Header_Container container-fluid col-sm-12">
+                            <div className="Content_Holder container">
+                                <h1>About DAK</h1>
+                            </div>
+                        </header>
+                        <div className="left_content_holder">
+                            <div className="left_content_paragraph">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quos, recusandae. Ex,
+                                    molestiae qui. Eius, voluptates debitis
+                                    atque totam optio modi fuga et inventore vel
+                                    obcaecati veniam ipsam quaerat velit
+                                    accusantium. Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Magni nihil
+                                    asperiores minus harum, quos expedita
+                                    nesciunt tempora. Quam voluptas maiores quas
+                                    minima fugit at consequuntur et aliquam, ut
+                                    necess itatibus? Reprehenderit!
+                                </p>
+                            </div>
+                            <div className="left_content_paragraph_image_holder">
+                                <img src={imageCard1} alt="Sesame Seed" />
+                            </div>
+                            <div className="left_content_paragraph">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quos, recusandae. Ex,
+                                    molestiae qui. Eius, voluptates debitis
+                                    atque totam optio modi fuga et inventore vel
+                                    obcaecati veniam ipsam quaerat velit.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <motion.div
+                        className="vertical-line1"
+                        ref={line1Ref}
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{
+                            opacity: line1InView ? 1 : 0,
+                            x: line1InView ? 0 : 100,
+                        }}
+                        transition={{ duration: 0.8, delay: 0.1 }}
+                    ></motion.div>
+                    <motion.div
+                        className="vertical-line2"
+                        ref={line2Ref}
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{
+                            opacity: line2InView ? 1 : 0,
+                            x: line2InView ? 0 : 100,
+                        }}
+                        transition={{ duration: 0.5, delay: 0.4 }}
+                    ></motion.div>
+                    <motion.div
+                        className="vertical-line3"
+                        ref={line3Ref}
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{
+                            opacity: line3InView ? 1 : 0,
+                            x: line3InView ? 0 : 100,
+                        }}
+                        transition={{ duration: 0.5, delay: 0.8 }}
+                    ></motion.div>
+                    <motion.div
+                        className="rightContent"
+                        ref={imageRef}
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{
+                            opacity: imageInView ? 1 : 0,
+                            scale: imageInView ? 1 : 0.8,
+                        }}
+                        transition={{ duration: 0.5, delay: 1.2 }}
+                    >
+                        <img src={aboutImage} alt="About DAK Trading PLC" />
+                    </motion.div>
                 </div>
-            </header>
-            <div className='left_content_holder'>
-                <div className='left_content_paragraph '>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, recusandae. Ex, molestiae qui. Eius,
-                    voluptates debitis atque totam optio modi fuga et inventore vel obcaecati veniam ipsam quaerat velit
-                    accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil asperiores minus
-                    harum, quos expedita nesciunt tempora. Quam voluptas maiores quas minima fugit at consequuntur et
-                    aliquam, ut necess itatibus? Reprehenderit!
-                    </p>
-                </div>
-                <div className='left_content_paragraph_image_holder'>
-                    <img src={imageCard1}></img>
-                </div>
-                <div className='left_content_paragraph '>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, recusandae. Ex, molestiae qui. Eius,
-                    voluptates debitis atque totam optio modi fuga et inventore vel obcaecati veniam ipsam quaerat velit.
-                    </p>
-                </div>
-            </div>
-          </div>
-          <div className="vertical-line1"></div>
-          <div className="vertical-line2"></div>
-          <div className="vertical-line3"></div>
-
-          <div className="rightContent">
-            <img src={aboutImage} alt="About DAK Trading PLC" /> 
-          </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default About;
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,30 +1,54 @@
-import './App.css';
+import "./App.css";
 
-function ContactForm(){
+function ContactForm() {
     return (
-        <div className='contactForm-container d-flex justify-content-between'>
-            <div className='Content_Holder'> 
+        <div className="contactForm-container d-flex justify-content-between">
+            <div className="Content_Holder">
                 <div class="row content_box">
-                    <div className='col-lg-6 col-md-12 col-sm-12 ContactForm-content d-flex flex-column'>
+                    <div className="col-lg-6 col-md-12 col-sm-12 ContactForm-content d-flex flex-column">
                         <h2>Contact US</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Deleniti repudiandae provident consequatur sapiente culpa
-                                nemo illo autem ipsum non veniam cupiditate ullam ex soluta quas optio, quidem quis iure obcaecati?
-                            </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Deleniti repudiandae provident consequatur
+                            sapiente culpa nemo illo autem ipsum non veniam
+                            cupiditate ullam ex soluta quas optio, quidem quis
+                            iure obcaecati?
+                        </p>
                     </div>
-                    <div className='contactForm-form col-lg-6 col-md-12 col-sm-12'>
-                    <div class="mb-3 mt-5">
-                        <label htmlFor="exampleFormControlInput1" className="form-label" style={{width:'100%', textAlign:'start'}}>Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div class="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label" style={{width:'100%',textAlign:'start'}}>Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
+                    <div className="contactForm-form col-lg-6 col-md-12 col-sm-12">
+                        <div class="mb-3 mt-5">
+                            <label
+                                htmlFor="exampleFormControlInput1"
+                                className="form-label"
+                                style={{ width: "100%", textAlign: "start" }}
+                            >
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="exampleFormControlInput1"
+                                placeholder="name@example.com"
+                            />
+                        </div>
+                        <div class="mb-3">
+                            <label
+                                htmlFor="exampleFormControlTextarea1"
+                                className="form-label"
+                                style={{ width: "100%", textAlign: "start" }}
+                            >
+                                Example textarea
+                            </label>
+                            <textarea
+                                className="form-control"
+                                id="exampleFormControlTextarea1"
+                                rows="3"
+                            ></textarea>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-export default ContactForm
+export default ContactForm;
